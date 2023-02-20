f5a9c4df3aad923e2d11801ddbea8e9c6073e7be
abolfazl omidvar <abolfazl.omidvar46@gmail.com>
2023-02-20 23:03:35 +0330

    kossher commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf("6144181414:AAH2isc8GPTSyQJM3nB7FwJkIWgCYyte14Q");
-bot.start((ctx) => ctx.reply('هاله اهل قبرس است :)))))))))))))\n حالا یدونه استیکر بفرست'));
-bot.on('sticker', (ctx) => ctx.reply(' بیلاخ 👍 \n :)))))))))))))))))))))))))))))))))))))))'));
+bot.start((ctx) => ctx.reply(".من یک بات بی‌خاصیت هستم"));
+
+bot.start((ctx) =>
+	ctx.reply(".سریع باش اگه میخوای زنده بمونی یک اسنیکر سمی بفرست")
+);
+
+bot.on("sticker", (ctx) => ctx.reply(".سمش کم بود 😢 ولی ایندفه کاریت ندارم"));
+
 bot.launch();
